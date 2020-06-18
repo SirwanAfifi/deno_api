e211143befd137fb0577932b8ed23cf3fa6db2e8
Sirwan <sir1afifi@gmail.com>
2020-06-18 14:00:00 +0100

    Remove nessie - it doesnt have good support for MySQL

---
+++ database/index.ts
@@ -0,0 +1,12 @@
+import { Database } from "https://deno.land/x/denodb/mod.ts";
+import "https://deno.land/x/dotenv/load.ts";
+
+console.log(Deno.env.get("DB"));
+
+export const DB = new Database("mysql", {
+  database: Deno.env.get("DB") || "test",
+  host: Deno.env.get("HOSTNAME") || "localhost",
+  username: Deno.env.get("USERNAME") || "",
+  password: Deno.env.get("PASSWORD") || "",
+  port: 3306,
+});
--- server.ts
+++ server.ts
@@ -2,16 +2,8 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import { applyGraphQL } from "https://deno.land/x/oak_graphql/mod.ts";
 import { RootTypes } from "./graphql/index.ts";
 import { Resolvers } from "./graphql/index.ts";
+import { DB } from "./database/index.ts";
 
 const app = new Application();
 
-const GraphQLService = await applyGraphQL({
-  typeDefs: RootTypes,
-  resolvers: Resolvers,
-});
-
-app.use(GraphQLService.routes(), GraphQLService.allowedMethods());
-
-console.log("Server listening at http://localhost:8080");
-
-await app.listen({ port: 8080 });
+await DB.sync();
